947594ac5c6830acb7b66ae068ca42aed6e8e452
SeogHyeon <jshjsh0413@gmail.com>
2022-08-22 01:56:26 +0900

    08220156

---
+++ lib/practice.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+int main(void){
+    ifstream readFile;
+    readFile.open("test.txt");//파일 열기     
+    if (readFile.is_open())
+    {
+        while (!readFile.eof())
+        {//1. istream의 getline.           
+        
+        string str;
+        getline(readFile,str);
+        cout<<str<<endl;
+        //지금은 읽은 문자열 바로 출력.        
+        }
+        readFile.close();//파일 닫아줍니다.    
+    }
+    return 0;
+}
+        
+
